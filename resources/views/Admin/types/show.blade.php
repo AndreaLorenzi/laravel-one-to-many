2ad484c872a28ee3da45dd867f02b0fc9af5fc8f
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-11 08:55:37 +0200

    mod show

--- resources/views/Admin/types/show.blade.php
+++ resources/views/Admin/types/show.blade.php
@@ -14,15 +14,7 @@
                 <th scope="row">{{ $type->name }}</th>
                 <td>{{ $type->description }}</td>
 
-                <td>
-                    <a class="btn btn-warning" href="{{ route('admin.type.edit', ['type' => $type->id]) }}">Edit</a>
-                    <form action="{{ route('admin.type.destroy', ['type' => $type->id]) }}" method="post"
-                        class="d-inline-block">
-                        @csrf
-                        @method('delete')
-                        <button class="btn btn-danger">Delete</button>
-                    </form>
-                </td>
+
             </tr>
         </tbody>
     </table>
